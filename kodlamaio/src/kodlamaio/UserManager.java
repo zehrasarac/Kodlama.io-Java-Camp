c56f6489dae3571dad7dedeb496a43da6e12c0f3
zehrasarac <60752226+zehrasarac@users.noreply.github.com>
2021-05-05 20:59:00 +0300

    Delete kodlamaio directory

--- kodlamaio/src/kodlamaio/UserManager.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio;
-
-public class UserManager {
-	public void Giris(User user) {
-		System.out.println(user.getEmail()+user.getPassword()+"Sifre ve mailiniz dogru! giris basarili");
-	}
-}
